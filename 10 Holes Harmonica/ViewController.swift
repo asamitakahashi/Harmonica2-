39a39c68bab5356a676545ad9967a958b6e0c323
asamitakahashi <1105asamit@i.softbank.jp>
2020-09-23 23:22:03 +0900

    初回

--- 10 Holes Harmonica/ViewController.swift
+++ 10 Holes Harmonica/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Picker: UIPickerView!
+    
+    let dataList = [
+        "C","Db","D","Eb","E","F","G","Ab","A","Bb","B",
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    // UIPickerViewの列の数
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // UIPickerViewの行数、リストの数
+    func pickerView(_ pickerView: UIPickerView,
+                    numberOfRowsInComponent component: Int) -> Int {
+        return dataList.count
+    }
+    
+    // UIPickerViewの最初の表示
+    func pickerView(_ pickerView: UIPickerView,
+                    titleForRow row: Int,
+                    forComponent component: Int) -> String? {
+        
+        return dataList[row]
     }
 
-
 }
+
+
+
+//1つ目は，キーの選択　カタカナ表記と英語表記  キー　ド　　ドれミふぁソらしド　キーC CDEFGABC
+//2つ目は，移動ドの表記．ドれミ表記か，CDE表記か，off．
 
